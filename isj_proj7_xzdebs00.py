08a5fddf9939eac9ef777f086595f1a205d52112
Kateryna Zdebska <94997872+zdebska@users.noreply.github.com>
2022-05-05 00:27:42 +0300

    Deleted tests from proj7

--- isj_proj7_xzdebs00.py
+++ isj_proj7_xzdebs00.py
@@ -13,29 +13,3 @@ def log_and_count(key = None, counts = None):
         return inner
         
     return log_and_count_decorator
-
-
-
-
-
-@log_and_count(key = 'basic functions', counts = my_counter)
-def f1(a, b=2):
-    return a ** b
-
-@log_and_count(key = 'basic functions', counts = my_counter)
-def f2(a, b=3):
-    return a ** 2 + b
-
-@log_and_count(counts = my_counter)
-def f3(a, b=5):
-    return a ** 3 - b
-
-f1(2)
-f2(2, b=4)
-f1(a=2, b=4)
-f2(4)
-f2(5)
-f3(5)
-f3(5,4)
-
-print(my_counter)
